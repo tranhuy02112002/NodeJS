1ce474d29facc32aeafeb1321b3beb83acb91942
HuyBBC <tranquanghuy02112002@gmail.com>
2023-08-15 08:00:50 +0700

    init project

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const port = 3000
+
+app.use(morgan('combined'))
+app.get('/tin-tuc', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
